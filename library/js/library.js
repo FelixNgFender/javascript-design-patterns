335b2395af2f682f018564d7b70870367a67c611
Felix Nguyen <ngthinh302@gmail.com>
2023-01-25 19:49:25 -0500

    File set up

--- library/js/library.js
+++ library/js/library.js
@@ -1,3 +1,5 @@
+const myLibrary = [];
+
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -6,4 +8,13 @@ function Book(title, author, pages, read) {
   this.info = function () {
     return `${title} by ${author}, ${pages} pages, ${read}`;
   };
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function displayBooks() {
+  const bookList = document.querySelector("#book-list");
+  
 }
