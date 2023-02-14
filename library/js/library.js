1a6ad502a8660754e404a1dd497543411c9852b7
Felix Nguyen <ngthinh302@gmail.com>
2023-02-13 22:58:31 -0500

    Class-based refactoring and JSDoc documentation for library.js

--- library/js/library.js
+++ library/js/library.js
@@ -1,20 +1,45 @@
+/**
+ * @fileoverview Library app
+ * @author Thinh Nguyen
+ * @version 1.5.0
+ */
+
+"use strict";
+
 // Data Structures
 const myLibrary = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.info = function info() {
-    return `${title} by ${author}, ${pages} pages, ${read}`;
-  };
+/** Class for Book object */
+class Book {
+  /**
+   * Create a book object.
+   * @param {string} title - The title of the book.
+   * @param {string} author - The author of the book.
+   * @param {number} pages - The number of pages in the book.
+   * @param {boolean} read - Whether the book has been read.
+   */
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+  }
+
+  /**
+   * Get the book info.
+   * @return {string} The book info.
+   */
+  info() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+  }
 }
 
-function toggleRead(book) {
+// Refactor to arrow functions
+
+const toggleRead = (book) => {
   const tempBook = book;
   tempBook.read = !tempBook.read;
-}
+};
 
 // User Interface
 const addBookBtn = document.getElementById("main-addBookBtn");
@@ -23,37 +48,37 @@ const modalOverlay = document.getElementById("modal-overlay");
 const modal = document.getElementById("modal");
 const errorMsg = document.getElementById("modal-errorMsg");
 
-function activateBookModal() {
+const activateBookModal = () => {
   addBookForm.reset();
   modal.classList.add("active");
   modalOverlay.classList.add("active");
-}
+};
 
-function closeBookModal() {
+const closeBookModal = () => {
   modal.classList.remove("active");
   modalOverlay.classList.remove("active");
   errorMsg.classList.remove("active");
-}
+};
 
 addBookBtn.addEventListener("click", activateBookModal);
 
 modalOverlay.addEventListener("click", closeBookModal);
 
-function isInLibrary(book) {
+const isInLibrary = (book) => {
   return myLibrary.some(
     (b) => b.title === book.title && b.author === book.author
   );
-}
+};
 
-function getBookFromInput() {
+const getBookFromInput = () => {
   const title = document.getElementById("title").value;
   const author = document.getElementById("author").value;
   const pages = document.getElementById("pages").value;
   const read = document.getElementById("isRead").checked;
   return new Book(title, author, pages, read);
-}
+};
 
-function displayBooks() {
+const displayBooks = () => {
   const bookGrid = document.getElementById("main-bookGrid");
   bookGrid.innerHTML = "";
   if (myLibrary.length !== 0) {
@@ -88,9 +113,9 @@ function displayBooks() {
       bookGrid.appendChild(bookCard);
     });
   }
-}
+};
 
-function addBook(e) {
+const addBook = (e) => {
   e.preventDefault();
   const newBook = getBookFromInput();
   if (isInLibrary(newBook)) {
@@ -101,7 +126,7 @@ function addBook(e) {
   displayBooks();
   closeBookModal();
   addBookForm.reset();
-}
+};
 
 addBookForm.onsubmit = addBook;
 
