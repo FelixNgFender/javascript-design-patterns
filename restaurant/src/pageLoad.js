5e9a72ba938fb28ac1297c021d2ee18ba6cd4868
Felix Nguyen <ngthinh302@gmail.com>
2023-02-21 01:15:21 -0500

    Add page load mechanisms

---
+++ restaurant/src/pageLoad.js
@@ -0,0 +1,79 @@
+/**
+ * @fileoverview Initialize page load
+ * @author Thinh Nguyen
+ * @version 1.0.0
+ */
+
+"use strict";
+
+/**
+ * Create the header component.
+ * @return {HTMLElement} Header element
+ */
+function header() {
+  const header = document.createElement("header");
+  const headerContainer = document.createElement("div");
+  const headerTitle = document.createElement("h1");
+  header.classList.add("header");
+  headerContainer.classList.add("header-container");
+  headerTitle.classList.add("header-title");
+  headerTitle.textContent = "Seishun Buta Yarou Restaurant";
+  headerContainer.appendChild(headerTitle);
+  header.appendChild(headerContainer);
+  return header;
+}
+
+/**
+ * Create the main component.
+ * @return {HTMLElement} Main element
+ */
+function main() {
+  const main = document.createElement("main");
+  const mainNavbar = document.createElement("nav");
+  const mainNavbarItems = ["Home", "Menu", "Contact"];
+  main.classList.add("main");
+  main.id = "main";
+  mainNavbar.classList.add("main-navbar");
+  mainNavbarItems.forEach((item) => {
+    const mainNavbarItem = document.createElement("button");
+    mainNavbarItem.classList.add("main-navbar-item");
+    mainNavbarItem.textContent = item;
+    mainNavbar.appendChild(mainNavbarItem);
+  });
+  main.appendChild(mainNavbar);
+  return main;
+}
+
+/**
+ * Create the footer component.
+ * @return {HTMLElement} Footer element
+ */
+function footer() {
+  const footer = document.createElement("footer");
+  const footerText = document.createElement("p");
+  const footerLink = document.createElement("a");
+  const footerIcon = document.createElement("i");
+  footer.classList.add("footer");
+  footerIcon.classList.add("fab", "fa-github");
+  footerText.textContent =
+    "Copyright © " + new Date().getFullYear() + " FelixNgFender";
+  footerLink.href = "https://github.com/FelixNgFender";
+  footerLink.target = "_blank";
+  footerLink.rel = "noopener noreferrer";
+  footerLink.appendChild(footerIcon);
+  footer.appendChild(footerText);
+  footer.appendChild(footerLink);
+  return footer;
+}
+
+/**
+ * Initialize page load.
+ * @return {void}
+ * @export
+ */
+export default function pageLoad() {
+  const content = document.getElementById("content");
+  content.appendChild(header());
+  content.appendChild(main());
+  content.appendChild(footer());
+}
